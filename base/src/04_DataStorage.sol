cba9bf9e82974467f20fc2c572fab7e71d8de863
xin <709464835@qq.com>
2024-08-20 22:21:07 +0800

    fix

--- base/src/04_DataStorage.sol
+++ base/src/04_DataStorage.sol
@@ -24,3 +24,12 @@ contract DataStorage {
     }
 
 } 
+
+contract Variables {
+     function global() external view returns(address, uint, bytes memory){
+        address sender = msg.sender;
+        uint blockNum = block.number;
+        bytes memory data = msg.data;
+        return(sender, blockNum, data);
+    }
+}
