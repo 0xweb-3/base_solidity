15785b644f23eab15601fa90f839cd46f1068eb4
xin <709464835@qq.com>
2024-08-25 19:50:10 +0800

    fix

---
+++ base/src/20.TransparentProxy.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+// 透明可升级合约的教学代码，不要用于生产。
+contract TransparentProxy {
+    address implementation; // logic合约地址
+    address admin; // 管理员
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    event CallFunResultEvent(bool, bytes);
+
+    // 构造函数，初始化admin和逻辑合约地址
+    constructor(address _implementation){
+        admin = msg.sender;
+        implementation = _implementation;
+    }
+
+    // fallback函数，将调用委托给逻辑合约
+    // 不能被admin调用，避免选择器冲突引发意外
+    fallback() external payable {
+        require(msg.sender != admin);
+        (bool success, bytes memory data) = implementation.delegatecall(msg.data);
+        emit CallFunResultEvent(success, data);
+    }
+
+    // 接收ETH时触发Received事件
+    receive() external payable {}
+
+    // 升级函数，改变逻辑合约地址，只能由admin调用
+    function upgrade(address newImplementation) external {
+        if (msg.sender != admin) revert();
+        implementation = newImplementation;
+    }
+}
+
+// 逻辑合约1
+contract Logic1 {
+    // 状态变量和proxy合约一致，防止插槽冲突
+    address public implementation; 
+    address public admin; 
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    // 改变proxy中状态变量 选择器： 0xc2985578
+    function foo() public {
+        words = "old";
+    }
+}
+
+// 逻辑合约2
+contract Logic2 {
+    // 状态变量和proxy合约一致，防止插槽冲突
+    address public implementation; 
+    address public admin; 
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    // 改变proxy中状态变量
+    function foo() public {
+        words = "xin";
+    }
+}
---
+++ base/src/21.UUPS.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+// UUPS的Proxy，跟普通的proxy像。
+// 升级函数在逻辑函数中，管理员可以通过升级函数更改逻辑合约地址，从而改变合约的逻辑。
+// 教学演示用，不要用在生产环境
+contract UUPSProxy {
+    address public implementation; // 逻辑合约地址
+    address public admin; // admin地址
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    event CallFunResultEvent(bool, bytes);
+
+
+    // 构造函数，初始化admin和逻辑合约地址
+    constructor(address _implementation){
+        admin = msg.sender;
+        implementation = _implementation;
+    }
+
+    // fallback函数，将调用委托给逻辑合约
+    fallback() external payable {
+        (bool success, bytes memory data) = implementation.delegatecall(msg.data);
+         emit CallFunResultEvent(success, data);
+    }
+
+    // 接收ETH时触发Received事件
+    receive() external payable {}
+}
+
+// 逻辑合约1
+contract Logic1 {
+    // 状态变量和proxy合约一致，防止插槽冲突
+    address public implementation; 
+    address public admin; 
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    // 改变proxy中状态变量 选择器： 0xc2985578
+    function foo() public {
+        words = "old";
+    }
+
+    // 升级函数，改变逻辑合约地址，只能由admin调用。选择器：0x0900f010
+    // UUPS中，逻辑函数中必须包含升级函数，不然就不能再升级了。
+    function upgrade(address newImplementation) external {
+        require(msg.sender == admin);
+        implementation = newImplementation;
+    }
+}
+
+// 逻辑合约2
+contract Logic2 {
+    // 状态变量和proxy合约一致，防止插槽冲突
+    address public implementation; 
+    address public admin; 
+    string public words; // 字符串，可以通过逻辑合约的函数改变
+
+    // 改变proxy中状态变量
+    function foo() public {
+        words = "xin";
+    }
+
+    // 升级函数，改变逻辑合约地址，只能由admin调用。选择器：0x0900f010
+    // UUPS中，逻辑函数中必须包含升级函数，不然就不能再升级了。
+    function upgrade(address newImplementation) external {
+        require(msg.sender == admin);
+        implementation = newImplementation;
+    }
+}
+
